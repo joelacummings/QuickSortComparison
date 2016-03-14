8f7545d96c850ed62bf848f154530ec631286ee4
Joel Cummings <joelcummings@mac.com>
2016-03-14 00:17:44 -0400

    Finish off Ada version of QuickSort

--- quicksort.adb
+++ quicksort.adb
@@ -1,13 +1,25 @@
+with Ada.Text_IO; use Ada;
 with Ada.Text_IO; use Ada.Text_IO;
+with Ada.Strings.Bounded;
 with Ada.integer_text_io; use Ada.integer_text_io;
 with ada.numerics.elementary_functions; use ada.numerics.elementary_functions;
+with Ada.Command_Line; use Ada.Command_Line;
 
 procedure quicksortproc is
 
-  nargs : integer;
-  type testNums is array(0..5) of integer;
+  array_size : Integer;
+  arrayPos : Integer;
+  FileName: String := Ada.Command_Line.Argument(1);
+  Integer_File : Text_IO.File_Type;
 
-  numbers: testNums;
+  type Node;
+  type List is access Node;
+  type Node is record
+    number: Integer;
+    next: List;
+  end record;
+  type testNums is array(Integer range <>) of integer;
+  numList: List;
 
   function partition(items: in out testNums; lo: integer; high: integer) return Integer is
     pivot: Integer := items(lo);
@@ -15,10 +27,7 @@ procedure quicksortproc is
     j: Integer := high + 1;
     temp: Integer := 0;
   begin
-
-
     loop
-
       loop
         i := i + 1;
 
@@ -55,20 +64,34 @@ procedure quicksortproc is
   end quicksort;
 
   -- main
+
   begin
 
+    Text_IO.Open(File=>Integer_File, Name=>FileName, Mode=>Text_IO.In_File);
+    array_size := 0;
 
+    while (not Text_IO.End_Of_File(Integer_File)) loop
+      array_size := array_size + 1;
+      numList := new Node'(number=>0, next=>numList);
+      Integer_Text_IO.Get(Integer_File, numList.number);
 
-    numbers(0) := 12;
-    numbers(1) := 5;
-    numbers(2) := 18;
-    numbers(3) := 4;
-    numbers(4) := 2;
-
-    quicksort(numbers, 0, 5);
-
-    for i in 0..4 loop
-      put_line(Integer'Image(numbers(i)));
     end loop;
 
+    declare
+      nums_array : testNums(0..array_size);
+    begin
+      arrayPos := 0;
+      while (numList.next /= null) loop
+        nums_array(arrayPos) := numList.number;
+        numList := numList.next;
+        arrayPos:= arrayPos + 1;
+      end loop;
+
+    quicksort(nums_array, 0, array_size);
+    --
+    for i in 0..array_size-1 loop
+      put_line(Integer'Image(nums_array(i)));
+    end loop;
+
+  end;
 end quicksortproc;
